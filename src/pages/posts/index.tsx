2bd6c5f14fe081195ff8fdb7d7026a8facb58a12
ericDK89 <ericthr42@gmail.com>
2022-05-11 03:58:15 -0300

    console.log data vem vazio

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,42 +2,37 @@ import { GetStaticProps } from "next";
 import Head from "next/head";
 import { getPrismicClient } from "../../services/prismic";
 import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+interface Post {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
-        <title>Post Ignews</title>
+        <title>Post | Ignews</title>
       </Head>
 
       <main className={styles.Container}>
         <div className={styles.Content}>
-          <a href="#">
-            <time>12 março de 2022</time>
-            <strong>Creating a monorepo with lerna e yarn workspace</strong>
-            <p>
-              In this guide you will lear n hot to acerat a monorepoo to mang e
-              mukltiple packagest weith a shared as
-            </p>
-          </a>
-          <a href="#">
-            <time>12 março de 2022</time>
-            <strong>Creating a monorepo with lerna e yarn workspace</strong>
-            <p>
-              In this guide you will lear n hot to acerat a monorepoo to mang e
-              mukltiple packagest weith a shared as
-            </p>
-          </a>
-          <a href="#">
-            <time>12 março de 2022</time>
-            <strong>Creating a monorepo with lerna e yarn workspace</strong>
-            <p>
-              In this guide you will lear n hot to acerat a monorepoo to mang e
-              mukltiple packagest weith a shared as
-            </p>
-          </a>
+          {/* {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time> {post.updatedAt} </time>
+              <strong> {post.title} </strong>
+              <p> {post.excerpt} </p>
+            </a>
+          ))} */}
         </div>
       </main>
     </>
@@ -47,12 +42,33 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
-  const res = await prismic.query([
-    Prismic.predicate.at("document.type", "myPostId"),
-    { fetch: ["myPostId.title", "myPostId.content"], pageSize: 100 },
-  ]);
+  const res = await prismic.query<any>(
+    [Prismic.predicates.at("document.type", "myPostId")],
+    {
+      fetch: ["myPostId.title", "myPostId.content"],
+      pageSize: 100,
+    }
+  );
 
-  console.log(res);
+  // const posts = res.results.map((post) => {
+  //   return {
+  //     slug: post.uid,
+  //     title: RichText.asText(post.data.title),
+  //     excerpt:
+  //       post.data.content.find((content) => content.type === "paragraph")
+  //         ?.text ?? "",
+  //     updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+  //       "pt-BR",
+  //       {
+  //         day: "2-digit",
+  //         month: "long",
+  //         year: "numeric",
+  //       }
+  //     ),
+  //   };
+  // });
+
+  console.log(JSON.stringify(res, null, 2));
 
   return {
     props: {},
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,7 +1,7 @@
 import Prismic from "@prismicio/client"
 
 export function getPrismicClient(req?: unknown) {
-  const prismic = Prismic.createClient(
+  const prismic = Prismic.client(
     process.env.PRISMIC_ENDPOINT,
     {
       req,

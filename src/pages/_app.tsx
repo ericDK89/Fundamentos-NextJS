25adcedfcaef01257ca09aa7d21d37a65e39920d
ericDK89 <ericthr42@gmail.com>
2022-05-11 19:22:19 -0300

    Subscription não salva no FaundaDB

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,28 +4,13 @@ import { Header } from "../components/Header";
 import { SessionProvider as NextAuthProvider } from "next-auth/react";
 
 import "../styles/global.scss";
-import Link from "next/link";
-import { PrismicProvider } from "@prismicio/react";
-import { PrismicPreview } from "@prismicio/next";
-import { linkResolver, repositoryName } from "../services/prismic";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PrismicProvider
-      linkResolver={linkResolver}
-      internalLinkComponent={({ href, children, ...props }) => (
-        <Link href={href}>
-          <a {...props}>{children}</a>
-        </Link>
-      )}
-    >
-      <PrismicPreview repositoryName={repositoryName}>
-        <NextAuthProvider session={pageProps.session}>
-          <Header />
-          <Component {...pageProps} />
-        </NextAuthProvider>
-      </PrismicPreview>
-    </PrismicProvider>
+    <NextAuthProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />
+    </NextAuthProvider>
   );
 }
 
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,9 +2,9 @@ import { GetStaticProps } from "next";
 import Head from "next/head";
 import { getPrismicClient } from "../../services/prismic";
 import Prismic from "@prismicio/client";
-import { RichText } from "prismic-dom";
 
 import styles from "./styles.module.scss";
+import Link from "next/link";
 
 interface Post {
   slug: string;
@@ -26,13 +26,15 @@ export default function Posts({ posts }: PostsProps) {
 
       <main className={styles.Container}>
         <div className={styles.Content}>
-          {/* {posts.map((post) => (
-            <a key={post.slug} href="#">
-              <time> {post.updatedAt} </time>
-              <strong> {post.title} </strong>
-              <p> {post.excerpt} </p>
-            </a>
-          ))} */}
+          {posts.map((post) => (
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a>
+                <time> {post.updatedAt} </time>
+                <strong> {post.title} </strong>
+                <p> {post.excerpt} </p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
@@ -43,34 +45,32 @@ export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
   const res = await prismic.query<any>(
-    [Prismic.predicates.at("document.type", "myPostId")],
+    [Prismic.predicates.at("document.type", "post")],
     {
-      fetch: ["myPostId.title", "myPostId.content"],
+      fetch: ["post.title", "post.content"],
       pageSize: 100,
     }
   );
 
-  // const posts = res.results.map((post) => {
-  //   return {
-  //     slug: post.uid,
-  //     title: RichText.asText(post.data.title),
-  //     excerpt:
-  //       post.data.content.find((content) => content.type === "paragraph")
-  //         ?.text ?? "",
-  //     updatedAt: new Date(post.last_publication_date).toLocaleDateString(
-  //       "pt-BR",
-  //       {
-  //         day: "2-digit",
-  //         month: "long",
-  //         year: "numeric",
-  //       }
-  //     ),
-  //   };
-  // });
-
-  console.log(JSON.stringify(res, null, 2));
+  const posts = res.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: post.data.title,
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: { posts },
   };
 };
